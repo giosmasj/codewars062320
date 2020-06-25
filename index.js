fbb4246d72d63a8acab6636fb760def5d68cad1c
James Giosmas <giosmasj@gmail.com>
2020-06-25 09:04:30 -0600

    solution to codewars problem 062520

--- index.js
+++ index.js
@@ -33,3 +33,17 @@ function filter_list(l) {
 function filter_list(l) {
   return l.filter( e => (typeof e != 'string'))
 }
+
+// Given an array of integers your solution should find the smallest integer.
+// For example:
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+// 8kyu
+// 06/25/2020
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+    }
+  }

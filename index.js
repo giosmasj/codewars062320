1530bdd812c9c1cccea9e34c0f8cfdf8e8efe001
James Giosmas <giosmasj@gmail.com>
2020-06-30 17:32:42 -0600

    solution and refactored solution to codewars problem from 06/30/2020

--- index.js
+++ index.js
@@ -79,3 +79,46 @@ function divisors(integer) {
   for (var i = 2; i <= Math.floor(integer / 2); ++i) if (integer % i == 0) res.push(i);
   return res.length ? res : integer + ' is prime'
 };
+
+// Bob is preparing to pass IQ test. The most frequent task in this
+// test is to find out which one of the given numbers differs from the
+// others. Bob observed that one number usually differs from the others
+// in evenness. Help Bob — to check his answers, he needs a program that
+// among the given numbers finds one that is different in evenness, and
+// return a position of this number.
+// ! Keep in mind that your task is to help Bob solve a real IQ test,
+// which means indexes of the elements start from 1 (not 0)
+// Test samples:
+// iqTest("2 4 7 8 10") => 3 Third number is odd, while the rest of the
+// numbers are even
+// iqTest("1 2 1 1") => 2 Second number is even, while the rest of the
+// numbers are odd
+// 6kyu
+// 06/30/2020
+
+function iqTest(numbers){
+  let numArr = numbers.split(' ');
+  let oddArr = [];
+  let evenArr = [];
+  for(let i = 0; i < numArr.length; i ++){
+    if(numArr[i] % 2) {
+      oddArr.push(i + 1)
+      } else {
+        evenArr.push(i + 1)
+      }
+    }
+    if(oddArr.length === 1) {
+      return oddArr[0]
+    } else {
+      return evenArr[0]
+    }
+}
+// Best Practice
+function iqTest(numbers){
+  numbers = numbers.split(" ").map(function(el){return parseInt(el)});
+  
+  var odd = numbers.filter(function(el){ return el % 2 === 1});
+  var even = numbers.filter(function(el){ return el % 2 === 0});
+  
+  return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
+}

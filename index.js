36d7ff1d38e425aa62168b5150140711dbe13012
James Giosmas <giosmasj@gmail.com>
2020-07-14 10:50:51 -0600

    solution to kata 07/14/2020

--- index.js
+++ index.js
@@ -138,3 +138,28 @@ function highAndLow(numbers){
   numbers = numbers.split(" ");
   return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers)
 }
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is positive.
+
+// Examples:
+
+// findNextSquare(121) --> returns 144
+// findNextSquare(625) --> returns 676
+// findNextSquare(114) --> returns -1 since 114 is not a perfect
+// 7kyu
+// 07/14/2020
+
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  return Math.sqrt(sq)%1===0 ? Math.pow(Math.sqrt(sq)+1,2) : -1
+}
+
+// best practice/refactored solution
+
+function findNextSquare(sq) {
+  return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
+}

c2121a58d8f50ee2e1972ca36d9de3996997ca44
James Giosmas <giosmasj@gmail.com>
2020-07-22 20:59:45 -0600

    solution and best practice solution tdeclare winner kata 07/22/2020

--- index.js
+++ index.js
@@ -243,3 +243,60 @@ function isValidWalk(walk) {
   function longest(s1, s2) {
     return Array.from(new Set(s1 + s2)).sort().join('')
   }
+
+// Create a function that returns the name of the winner in a fight
+// between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills
+// the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class
+// below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python)
+// will be integers larger than 0. You can mutate the Fighter objects.
+
+// Example:
+// function Fighter(name, health, damagePerAttack) {
+//         this.name = name;
+//         this.health = health;
+//         this.damagePerAttack = damagePerAttack;
+//         this.toString = function() { return this.name; }
+// }
+// 7kyu
+// 07/22/2020
+// I sold my car today!!!!
+
+function declareWinner(fighterA, fighterB, firstAttacker) {
+  const args = Array.from(arguments);
+  const fighters =  args.filter(arg => !arg[2]);  
+  firstAttacker = fighters.find(fighter => fighter.name === firstAttacker);
+  const secondAttacker = fighters.find(fighter => fighter.name != firstAttacker);
+
+  do {
+    secondAttacker.health = secondAttacker.health - firstAttacker.damagePerAttack;
+    if (secondAttacker.health <= 0) {
+      return firstAttacker.name;
+    } else {
+      firstAttacker.health = firstAttacker.health - secondAttacker.damagePerAttack;
+    }
+    if (firstAttacker.health <= 0) {
+      return secondAttacker.name;
+    }
+  }
+  while (secondAttacker.health >= 0 || firstAttacker.health >= 0 ); 
+}
+
+// refactored
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+  var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+  if(fac1 < fac2) {
+    return fighter2.name;
+  } else if(fac2 < fac1) {
+    return fighter1.name;
+  } else {
+    return firstAttacker;
+  }
+}

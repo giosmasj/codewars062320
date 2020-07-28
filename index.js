ac214ce4d92d8f3512bcc55a2085db85b96322b1
James Giosmas <giosmasj@gmail.com>
2020-07-28 10:42:43 -0600

    isIsogram 7kyu kata, best practice, and clever solution

--- index.js
+++ index.js
@@ -361,3 +361,39 @@ String.prototype.toJadenCase = function () {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }).join(" ");
 }
+
+// An isogram is a word that has no repeating letters, consecutive or
+// non-consecutive. Implement a function that determines whether a string
+// that contains only letters is an isogram. Assume the empty string is an
+// isogram. Ignore letter case.
+
+// isIsogram("Dermatoglyphics") == true
+// isIsogram("aba") == false
+// isIsogram("moOse") == false // -- ignore letter case
+// 7/28/2020
+// 7kyu
+
+function isIsogram(str){
+  let i, j;
+  str = str.toLowerCase();
+  for(i = 0; i < str.length; i++) {
+    for(j = i + 1; j < str.length; j++) {
+      if(str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// best practice
+
+function isIsogram(str){ 
+  return !/(\w).*\1/i.test(str)
+}
+
+// clever solution
+
+function isIsogram(str){
+  return new Set(str.toUpperCase()).size == str.length;
+}

0b39d8d67cbdf1dded8a3c9b8e869a9e7aca33ec
James Giosmas <giosmasj@gmail.com>
2020-08-20 15:33:11 -0600

    completed uniqueInOrder kata in JavaScript as well, gotta remember .push in JS .append in Python

--- index.js
+++ index.js
@@ -409,3 +409,22 @@ function longest(s1, s2) {
   return Array.from(new Set(s1 + s2)).sort().join('')
 }
 
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder = function(iterable){
+  let uniqes = []
+  let char
+
+  for (let i = 0; i < iterable.length; i++){
+    if (iterable[i] !== char){
+      uniqes.push(char = iterable[i])
+    }
+  }
+  return uniques
+}

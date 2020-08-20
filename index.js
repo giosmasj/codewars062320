0d4fb925e13957221beb276ccffe9dd733d972f9
James Giosmas <giosmasj@gmail.com>
2020-08-20 15:49:31 -0600

    added best practice which was basically the same as my solution and added clever one liner solution

--- index.js
+++ index.js
@@ -428,3 +428,24 @@ var uniqueInOrder = function(iterable){
   }
   return uniques
 }
+
+// above solution is basically best practice
+
+function uniqueInOrder(it){
+  var result = []
+  var last
+
+  for (var i = 0; i < it.length; i++) {
+    if (it[i] !== last) {
+      result.push(last = it[i])
+    }
+  }
+
+  return result
+}
+
+// clever short solution
+
+var uniqueInOrder = function(iterable){
+  return [...iterable].filter((a, i) => a !== iterable[i-1])
+}
